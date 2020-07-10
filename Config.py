bd32d3b437c486f89b31c11bd6e0a61c8c70afb9
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-07-09 21:26:48 -0400

    Add Localstorage

--- Config.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "verify_certs" : "True",
-    "learn_rest_url" : "devcon.blackboard.com",
-    "learn_rest_key" : "bb4a23dc-9a57-414e-b0c7-10d19e061071",
-    "learn_rest_secret" : "as2ITQPcMJAClh1H51Krc2JGwpowDXBI",
-    "app_url" : "https://bbdn-uef-python.herokuapp.com"
-}
