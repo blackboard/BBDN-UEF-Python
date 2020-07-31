762d19daee7543318b7c8103eace347dd70b8adc
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-07-31 14:43:51 -0400

    Fix port and change integration.js file name

--- app/app.py
+++ app/app.py
@@ -217,5 +217,4 @@ def authcode():
 
 if __name__ == '__main__':
     restAuthController = None
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
